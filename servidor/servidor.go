5794137a927b81ea47542f3c53c623eb2c7f5ead
Kiril Gaydarov <kvg1@alu.ua.es>
2019-02-26 14:07:40 +0100

    Cliente y servidor separados en directorios distintos

--- servidor/servidor.go
+++ servidor/servidor.go
@@ -19,7 +19,6 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
 )
 
 // función para comprobar errores (ahorra escritura)
@@ -30,26 +29,8 @@ func chk(e error) {
 }
 
 func main() {
+	fmt.Println("Entrando en modo servidor...")
 
-	fmt.Println("cnx.go :: un ejemplo para gestionar conexiones TCP en Go.")
-	if len(os.Args) > 1 {
-		switch os.Args[1] {
-		case "srv":
-			fmt.Println("Entrando en modo servidor...")
-			server()
-		case "cli":
-			fmt.Println("Entrando en modo cliente...")
-			client()
-		default:
-			fmt.Println("Parámetro '", os.Args[1], "' desconocido. Introduce srv para funcionalidad de servidor y cli para funcionalidad de cliente")
-		}
-	} else {
-		fmt.Println("Introduce srv para funcionalidad de servidor y cli para funcionalidad de cliente")
-	}
-}
-
-// gestiona el modo servidor
-func server() {
 	ln, err := net.Listen("tcp", "localhost:1337") // escucha en espera de conexión
 	chk(err)
 	defer ln.Close() // nos aseguramos que cerramos las conexiones aunque el programa falle
@@ -75,22 +56,4 @@ func server() {
 			fmt.Println("cierre[", port, "]")
 		}()
 	}
-}
-
-func client() {
-	conn, err := net.Dial("tcp", "localhost:1337") // llamamos al servidor
-	chk(err)
-	defer conn.Close() // es importante cerrar la conexión al finalizar
-
-	fmt.Println("conectado a ", conn.RemoteAddr())
-
-	keyscan := bufio.NewScanner(os.Stdin) // scanner para la entrada estándar (teclado)
-	netscan := bufio.NewScanner(conn)     // scanner para la conexión (datos desde el servidor)
-
-	for keyscan.Scan() { // escaneamos la entrada
-		fmt.Fprintln(conn, keyscan.Text())         // enviamos la entrada al servidor
-		netscan.Scan()                             // escaneamos la conexión
-		fmt.Println("servidor: " + netscan.Text()) // mostramos mensaje desde el servidor
-	}
-
 }
